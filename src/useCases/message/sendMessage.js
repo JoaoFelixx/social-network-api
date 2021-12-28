6011b92ac30c3cfeba4b8f152b87840f8d439ebe
Felix developer <felixxdeveloper@gmail.com>
2021-12-28 16:10:30 -0300

    Added the socketio at project

--- src/useCases/message/sendMessage.js
+++
@@ -1,7 +0,0 @@
-const { createConnection } = require('net');
-
-const client = createConnection(4520, '192.168.3.21');
-
-client.on('connect', () => console.log('Connected the connection'));
-
-client.on('data', (message) => console.log('message: ',message.toString()));
